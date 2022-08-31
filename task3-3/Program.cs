88e7f5c274ea3a04c4df58fb1345687e7d88e359
Protiana <protiana@yandex.ru>
2022-08-31 23:09:32 +0300

    task 3 from homework 3

---
+++ task3-3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает число N и возвращает таблицукубов чисел от 1 до N
+
+
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+
+string FindCube(int n)
+{
+    string result = "";
+    for (int i = 1; i <= n; i++)
+    {
+        result += $"{Math.Pow(i, 3)} ";
+    }
+    return result;
+}
+
+//string Cubs = FindCube(N);
+Console.WriteLine(FindCube(N));
+
