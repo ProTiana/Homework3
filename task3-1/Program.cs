0d165b917f0f771c3b3e604ea653f737652d7f85
Protiana <protiana@yandex.ru>
2022-08-31 23:07:19 +0300

    task 1 from homework 3

---
+++ task3-1/Program.cs
@@ -0,0 +1,26 @@
+﻿// принять на вход пятизначное число и проверить является ли оно палиндромом
+
+Console.WriteLine("Введите пятизначное число");
+string numbers = Convert.ToString(Console.ReadLine());
+
+bool Cheсk (string n){
+
+ if(n[0]==n[4] && n[1]==n[3]) {
+    return true;
+}
+  else{
+    return false;
+  }
+}
+if (numbers.Length <= 5){
+bool x = Cheсk(numbers);
+if (x == true){
+    Console.WriteLine("Это палиндром");
+}
+else{
+    Console.WriteLine("Не палиндром");
+}
+}
+else{
+    Console.WriteLine("Ошибка. Введите пятизначное число");
+}
